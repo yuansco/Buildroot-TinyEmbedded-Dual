1a01a4149e1e5dfbd5830519d03e2e358cae09f5
Yu-An Chen <aazz99770@gmail.com>
2024-03-23 21:20:43 +0800

    Add board utility: motion_angle

---
+++ buildroot/package/motion_angle/motion_angle.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# motion_angle
+#
+################################################################################
+
+MOTION_ANGLE_VERSION = 7aba185098a24ccb2d8e3958cdf4573a097244d4
+MOTION_ANGLE_SITE = https://github.com/yuansco/Motion-Angle.git
+MOTION_ANGLE_SITE_METHOD=git
+
+define MOTION_ANGLE_BUILD_CMDS
+	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS)
+endef
+
+define MOTION_ANGLE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/motion_angle $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/gsensor_show.sh $(TARGET_DIR)/root
+endef
+
+$(eval $(generic-package))
+
